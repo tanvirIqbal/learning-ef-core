5d4efc283b3e86f54410222b98be150309e41c13
tanviriqbal <tanviriqbal4u@live.com>
2023-07-28 12:36:51 +1000

    Add Author, Publisher, SubCategory Classes

--- CodingWiki_DataAccess/Data/ApplicationDBContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDBContext.cs
@@ -12,6 +12,9 @@ namespace CodingWiki_DataAccess.Data
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
