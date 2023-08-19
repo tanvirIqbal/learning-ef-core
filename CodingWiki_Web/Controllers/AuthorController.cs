628231f05778fb2a9366bdd973d1b8e9d5a6abe2
tanviriqbal <tanviriqbal4u@live.com>
2023-08-20 09:41:41 +1000

    Author and Publisher CRUD

---
+++ CodingWiki_Web/Controllers/AuthorController.cs
@@ -0,0 +1,73 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        public AuthorController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Author> authors = _context.Authors.ToList();
+            return View(authors);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Author author = new();
+            if (id == null || id == 0)
+            {
+                return View(author);
+            }
+            author = _context.Authors.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.Author_Id == 0)
+                {
+                    await _context.AddAsync(author);
+                }
+                else
+                {
+                    _context.Authors.Update(author);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(author);
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Author author = new();
+            author = _context.Authors.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            _context.Authors.Remove(author);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CodingWiki_Web/Controllers/PublisherController.cs
@@ -0,0 +1,73 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        public PublisherController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Publisher> publishers = _context.Publishers.ToList();
+            return View(publishers);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            Publisher publisher = new();
+            if (id == null || id == 0)
+            {
+                return View(publisher);
+            }
+            publisher = _context.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                if (publisher.Publisher_Id == 0)
+                {
+                    await _context.AddAsync(publisher);
+                }
+                else
+                {
+                    _context.Publishers.Update(publisher);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(publisher);
+            }
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Publisher publisher = new();
+            publisher = _context.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            _context.Publishers.Remove(publisher);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
